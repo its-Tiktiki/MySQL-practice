a8aa49c7d6fb6e5e7d6231c3fadbe02064ecef1c
its-Tiktiki <itzpomodoro79@gmail.com>
2026-08-08 16:31:51 +0600

    add another practice file of MySQL

---
+++ practice2.sql
@@ -0,0 +1,15 @@
+create database college;
+use college;
+create table student(
+	rollno int primary key,
+    name varchar(50)
+);
+
+select * from student;
+
+insert into student
+(rollno, name)
+values
+(101, "rohan"),
+(102, "alif"),
+(103, "rizvi");
