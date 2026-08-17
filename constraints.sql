b1de80fe6719d662cad058f41f8aa9346aa4fb11
its-Tiktiki <itzpomodoro79@gmail.com>
2026-08-17 21:37:20 +0600

    update file

--- constraints.sql
+++ constraints.sql
@@ -1,6 +1,8 @@
+// create database
 create database country;
 use country;
 
+// create table
 create table city(
     id int primary key,
     city varchar(50),
@@ -8,6 +10,7 @@ create table city(
     constraint age_check check(age >= 18 and city="Jashore")
 );
 
+// create new table
 create table newTab(
     age int check(age >= 18)
 );
